7a6db588e7add174cc8794412f4f66bd5f8794f1
Jan <wennmann@protonmail.com>
2021-01-28 23:08:39 +0100

    Add basic and untested HTTP request handling

--- stock_scraper/src/main.rs
+++ stock_scraper/src/main.rs
@@ -1,3 +1,23 @@
+extern crate reqwest;
+extern crate scraper;
+
+use reqwest::Response;
+use scraper::{Html, Selector};
+
 fn main() {
     println!("Hello, world!");
+}
+
+// Basic HTTP handling -> put it in a class later on
+async fn scrape_from_url(url: &str) -> Response {
+    let response: Response = reqwest::get(url).await.unwrap(); // never use .unwrap() for other than development purposes
+    assert!(response.status().is_success()); // log other status codes
+
+    response
+}
+
+async fn extract_body(response: Response) ->  String {
+    let body = response.text().await.unwrap(); // why is everything async?
+
+    body
 }
