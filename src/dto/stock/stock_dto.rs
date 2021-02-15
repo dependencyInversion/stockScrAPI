e1fcc06e26c829e11698865195b3e8e111f1a5d7
dependencyInversion <wennmann@protonmail.com>
2021-02-15 23:21:07 +0100

    Add missing member; Fix naming; Add enum of column names

--- src/dto/stock/stock_dto.rs
+++ src/dto/stock/stock_dto.rs
@@ -1,16 +1,29 @@
+enum Columns {
+    Symbol,
+    Name,
+    Price,
+    Change,
+    ChangeInPercentage,
+    Volume,
+    AverageVolume,
+    MarketCap,
+    PriceEarningsRatio
+}
+
 pub struct stock_dto {
-    symbol: String,
+    pub symbol: String,
     name: String,
     price: f32,
     change: f32,
     change_in_percentage: f32,
     volume: i64,
+    average_volume: i64,
     market_cap: i64,
-    pe_ratio_ttm: f32,
+    price_earnings_ratio: f32,
 }
 
 impl stock_dto {
-    pub fn new ( // move this into a factory; input (vec<vec<str>>); output (list<stock_dto>)
+    pub fn new (
 
         symbol: &str,
         name: &str,
@@ -18,8 +31,9 @@ impl stock_dto {
         change: &str,
         change_in_percentage: &str,
         volume: &str,
+        average_volume: &str,
         market_cap: &str,
-        pe_ratio_ttm: &str,
+        price_earnings_ratio: &str,
 
     ) -> stock_dto {
 
@@ -29,18 +43,20 @@ impl stock_dto {
         let change = change.parse::<f32>().unwrap();
         let change_in_percentage = change_in_percentage.parse::<f32>().unwrap();
         let volume = volume.parse::<i64>().unwrap();
+        let average_volume = average_volume.parse::<i64>().unwrap();
         let market_cap = market_cap.parse::<i64>().unwrap();
-        let pe_ratio_ttm = pe_ratio_ttm.parse::<f32>().unwrap();
+        let price_earnings_ratio = price_earnings_ratio.parse::<f32>().unwrap();
 
         stock_dto {
             symbol,
             name,
             price,
             change,
-            change_in_percentage,
+            change_in_percentage, // cut off percent sign
             volume,
+            average_volume,
             market_cap,
-            pe_ratio_ttm,
+            price_earnings_ratio, // expect 'N/A'
         }
     }
 }
