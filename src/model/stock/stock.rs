4bb989e369009c889c6565d9a252ba8dcfb0909e
dependencyInversion <wennmann@protonmail.com>
2021-02-17 23:25:13 +0100

    Integrate new models as member of the stock model

--- src/model/stock/stock.rs
+++ src/model/stock/stock.rs
@@ -1,12 +1,15 @@
 use crate::symbol::Symbol;
 use crate::name::Name;
+use crate::price::Price;
+use crate::change::Change;
+use crate::change_in_percentage::ChangeInPercentage;
 
-pub struct stock {
+pub struct Stock {
     pub symbol: Symbol,
     pub name: Name,
-    pub price: f32,
-    pub change: f32,
-    pub change_in_percentage: f32,
+    pub price: Price,
+    pub change: Change,
+    pub change_in_percentage: ChangeInPercentage,
     pub volume: i64,
     pub average_volume: i64,
     pub market_cap: i64,
