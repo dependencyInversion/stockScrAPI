e26bf6c6aee53da7ab0060321a18ba96163c5f9b
dependencyInversion <wennmann@protonmail.com>
2021-02-17 23:26:06 +0100

    Integrating new models

--- src/model/util/stock/stock_dto_factory.rs
+++ src/model/util/stock/stock_dto_factory.rs
@@ -1,7 +1,11 @@
-use crate::stock_dto::stock_dto;
+use price::Price;
+
+use crate::{price, stock::Stock};
 use crate::short_scale_resolver::short_scale_resolver;
 use crate::symbol::Symbol;
 use crate::name::Name;
+use crate::change::Change;
+use crate::change_in_percentage::ChangeInPercentage;
 
 pub struct stock_dto_factory { }
 
@@ -10,19 +14,19 @@ impl stock_dto_factory {
         stock_dto_factory{ }
     }
 
-    pub fn from_string_slice_vector(&self, raw_stock_data: &Vec<&str>, resolver: &short_scale_resolver) -> stock_dto {
+    pub fn from_string_slice_vector(&self, raw_stock_data: &Vec<&str>, resolver: &short_scale_resolver) -> Stock {
         
-        let symbol = Symbol::from_string_slice(raw_stock_data[0]).get_symbol();
-        let name = Name::from_string_slice(raw_stock_data[1]).get_name();
-        let price = raw_stock_data[2].parse::<f32>().unwrap();
-        let change = raw_stock_data[3].parse::<f32>().unwrap();
-        let change_in_percentage = raw_stock_data[4].parse::<f32>().unwrap();
+        let symbol = Symbol::from_string_slice(raw_stock_data[0]);
+        let name = Name::from_string_slice(raw_stock_data[1]);
+        let price = Price::from_string_slice(raw_stock_data[2]);
+        let change = Change::from_string_slice(raw_stock_data[3]);
+        let change_in_percentage = ChangeInPercentage::from_string_slice_with_trailing_sign(raw_stock_data[4]);
         let volume = resolver.from_string_slice(raw_stock_data[5]);
         let average_volume = resolver.from_string_slice(raw_stock_data[6]);
         let market_cap = resolver.from_string_slice(raw_stock_data[7]);
         let price_earnings_ratio = raw_stock_data[8].parse::<f32>().unwrap();
 
-        stock_dto {
+        Stock {
             symbol,
             name,
             price,
