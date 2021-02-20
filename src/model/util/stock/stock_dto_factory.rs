db7d4380aaabb62931f4dba6053fb60f9426e6f5
dependencyInversion <wennmann@protonmail.com>
2021-02-20 22:17:36 +0100

    Add new properties

--- src/model/util/stock/stock_dto_factory.rs
+++ src/model/util/stock/stock_dto_factory.rs
@@ -1,11 +1,7 @@
-use price::Price;
+#[path = "../../stock/stock.rs"] mod stock;
 
-use crate::{price, stock::Stock};
-use crate::short_scale_resolver::short_scale_resolver;
-use crate::symbol::Symbol;
-use crate::name::Name;
-use crate::change::Change;
-use crate::change_in_percentage::ChangeInPercentage;
+use stock::{ Stock, AverageVolume, ChangeInPercentage, Change, MarketCap, Name, Price, Volume, Symbol };
+use crate::short_scale_resolver::ShortScaleResolver;
 
 pub struct stock_dto_factory { }
 
@@ -14,16 +10,16 @@ impl stock_dto_factory {
         stock_dto_factory{ }
     }
 
-    pub fn from_string_slice_vector(&self, raw_stock_data: &Vec<&str>, resolver: &short_scale_resolver) -> Stock {
+    pub fn from_string_slice_vector(&self, raw_stock_data: &Vec<&str>, resolver: &ShortScaleResolver) -> Stock {
         
         let symbol = Symbol::from_string_slice(raw_stock_data[0]);
         let name = Name::from_string_slice(raw_stock_data[1]);
         let price = Price::from_string_slice(raw_stock_data[2]);
         let change = Change::from_string_slice(raw_stock_data[3]);
         let change_in_percentage = ChangeInPercentage::from_string_slice_with_trailing_sign(raw_stock_data[4]);
-        let volume = resolver.from_string_slice(raw_stock_data[5]);
-        let average_volume = resolver.from_string_slice(raw_stock_data[6]);
-        let market_cap = resolver.from_string_slice(raw_stock_data[7]);
+        let volume = Volume::from_string_slice(raw_stock_data[5]);
+        let average_volume = AverageVolume::from_string_slice(raw_stock_data[6]);
+        let market_cap = MarketCap::from_string_slice(raw_stock_data[7]);
         let price_earnings_ratio = raw_stock_data[8].parse::<f32>().unwrap();
 
         Stock {
@@ -31,7 +27,7 @@ impl stock_dto_factory {
             name,
             price,
             change,
-            change_in_percentage, // cut off percent sign
+            change_in_percentage,
             volume,
             average_volume,
             market_cap,
